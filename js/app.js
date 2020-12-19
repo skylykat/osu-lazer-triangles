466b8681522389aa83a20edffa5ee2cf32e8b112
skyrawr <6skylarstevens9@gmail.com>
2020-12-20 00:30:22 +0200

    stupid

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 particlesJS.load('particles-js', 'particles.json');
-var base = 1.2;
+var base = 1;
 var speed = 5;
 //simulate beat or something
 setInterval(function(){
@@ -8,7 +8,7 @@ setInterval(function(){
 }, 1000);
 // do fun math stuff
 setInterval(() => {
-	speed = speed/base
+	speed = speed/1.2
 	//console.log(speed.toFixed(2));
-	window.pJSDom[0].pJS.particles.move.speed = speed+1;
+	window.pJSDom[0].pJS.particles.move.speed = speed+base;
 }, 100);
